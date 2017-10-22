6229e29d0cd39e53acd383ebbc2716932e8386cf
Jerrell064 <31287613+Jerrell064@users.noreply.github.com>
2017-10-22 10:11:59 -0700

    Add files via upload

---
+++ Lab 6/action.js
@@ -0,0 +1,40 @@
+function checkEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+
+}
+
+function myAjaxFunction() {
+    alert("Your Ajax has been made")
+    document.getElementsByClassName("subform").reset()
+}
+
+function checkName() {
+    var regEx = new RegExp('\<');
+    if(regEx.test(document.getElementById('name').value)) {
+        document.getElementById('name').style.backgroundColor = "red"
+    } else {
+        document.getElementById('name').style.backgroundColor = ""
+    }
+}
+
+function checkAge() {
+    var regEx = new RegExp('\[0-9]');
+    var age = document.getElementById('age').value
+    if(regEx.test(age)) {
+       document.getElementById('age').style.backgroundColor = "red"
+    } else {
+        document.getElementById('age').style.backgroundColor = ""
+    }
+}
+
+function validate() {
+    var valid = checkEmail(document.getElementById(email).value)
+    console.log(valid)
+    if(valid == true) {
+        myAjaxFunction()
+    }
+    else {
+        alert("The information is not correct. Please check it")
+    } 
+}
